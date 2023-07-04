4e3b0c015b66768eda91662a3187db5ad92be4f6
Michał Mizia <mmizia05@gmail.com>
2023-07-04 21:51:23 +0200

    Updated one error

--- src/lib/state/media-queries-generation.ts
+++ src/lib/state/media-queries-generation.ts
@@ -1,25 +1,25 @@
-import { useEffect } from "react";
-// import { Dispatch, SetStateAction } from "react";
-import { create } from "zustand";
+// import { useEffect } from "react";
+// // import { Dispatch, SetStateAction } from "react";
+// import { create } from "zustand";
 
-interface GenerationState {
-  isMobile: boolean;
-  update: () => void;
-}
+// interface GenerationState {
+//   isMobile: boolean;
+//   update: () => void;
+// }
 
-const mobileQuery = "(max-width: 660px)";
+// const mobileQuery = "(max-width: 660px)";
 
-export const useQueryStore = create<GenerationState>()((set, get) => ({
-  isMobile: false,
-  update: () => {
-    useEffect(() => {
-      const media = window.matchMedia(mobileQuery);
-      if (media.matches !== get().isMobile) {
-        set({ isMobile: media.matches });
-      }
-      const listener = () => set({ isMobile: media.matches });
-      window.addEventListener("resize", listener);
-      return () => window.removeEventListener("resize", listener);
-    }, []);
-  },
-}));
+// export const useQueryStore = create<GenerationState>()((set, get) => ({
+//   isMobile: false,
+//   update: () => {
+//     useEffect(() => {
+//       const media = window.matchMedia(mobileQuery);
+//       if (media.matches !== get().isMobile) {
+//         set({ isMobile: media.matches });
+//       }
+//       const listener = () => set({ isMobile: media.matches });
+//       window.addEventListener("resize", listener);
+//       return () => window.removeEventListener("resize", listener);
+//     }, []);
+//   },
+// }));
